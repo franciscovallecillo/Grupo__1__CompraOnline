1c64fd72e4e4c3ead510a55ac314fbb29933711b
nanofleck <fernafleck@gmail.com>
2020-07-09 20:27:50 -0300

    users.json registro e inicio de sesion

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,15 +1,61 @@
 const path = require("path");
+const fs = require('fs');
+const bcrypt = require('bcrypt');
+
 
 const controlador = {
     profile: (req,res)=>{
-        res.render(path.resolve(__dirname,"../views/user.ejs"));
+        res.render(path.resolve(__dirname,"../views/user.ejs"));        
     },
-    registro:(req,res)=>{
+
+    profileEmpty: (req,res)=>{
+        res.render(path.resolve(__dirname,"../views/user2.ejs"));
+    },
+
+    formularioRegistro:(req,res)=>{
         res.render(path.resolve(__dirname,"../views/formularios/altaUsuario.ejs"));
     },
-    login: (req,res)=>{
+
+    registro:(req,res)=>{
+        let usuarios = JSON.parse(fs.readFileSync(path.resolve(__dirname,'../models/users.json'), {encoding: 'utf-8'}));
+
+        let idNuevo = usuarios.length+1;
+
+        let nuevoUsuario = {
+            id: idNuevo,
+            nombre: req.body.nombre,
+            apellido: req.body.apellido,
+            email: req.body.email,
+            contraseña: bcrypt.hashSync(req.body.password,10)
+        }
+
+        usuarios.push(nuevoUsuario);
+
+        usuariosJson = JSON.stringify(usuarios,null,2);
+
+        fs.writeFileSync(path.resolve(__dirname, '../models/users.json'),usuariosJson);
+
+        let usuarioRegistrado = nuevoUsuario;
+
+        res.render(path.resolve(__dirname,"../views/user.ejs"), {usuarioRegistrado});
+    },
+    
+    pageLogin: (req,res)=>{
         res.render(path.resolve(__dirname,"../views/login.ejs"));
     },
+
+    login: (req,res)=>{
+        let usuarios = JSON.parse(fs.readFileSync(path.resolve(__dirname,'../models/users.json'), {encoding: 'utf-8'}));
+
+        for ( let i = 0; i < usuarios.length ; i++){
+            if ( req.body.email == usuarios[i].email){
+                let usuarioRegistrado = usuarios[i];
+                res.render(path.resolve(__dirname,"../views/user.ejs"), {usuarioRegistrado});
+            }else{
+                console.log("Usuario no encontrado");
+            }
+        }
+    }
 };
 
 module.exports = controlador;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,7 +4,11 @@ const path = require("path");
 const userController = require(path.resolve(__dirname,"../controllers/userController.js"));
 
 router.get("/user", userController.profile);
-router.get("/altaUsuario", userController.registro);
-router.get("/login", userController.login);
+router.get("/user2", userController.profileEmpty);
+//router.post("/user", userController.profile);
+router.get("/altaUsuario", userController.formularioRegistro);
+router.post("/altaUsuario", userController.registro);
+router.get("/login", userController.pageLogin);
+router.post("/login", userController.login);
 
 module.exports = router;
